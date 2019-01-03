696dd3e9c7925d93f86b7bbbed598c9cbe483d18
MulverineX <mulverin3@gmail.com>
2019-01-03 02:00:37 -0700

    Starting manifest & more mpm work

--- src/build/mpm/index.ts
+++ src/build/mpm/index.ts
@@ -1,3 +1,14 @@
-export function mpm(packs) {
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import { pack } from './../../classes/pack'
+import { mpm_model } from './../../classes/json_schema/mpm_model'
+import { model } from './../../classes/json_schema/minecraft/model'
+
+export function mpm(packs: Array<pack>) {
+    let pack_ids: object = {};
+    for (let pack of packs) {
+        const pack_id = { [pack.id]: pack.name };
+        pack_ids = { ...pack_ids, ...pack_id }
+    }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as fs from "fs-extra"
 import * as path from "path"
-import { pack } from "./classes/pack"
+import * as hjson from "hjson"
+
 import * as setup_output from "./build/setup_output/index"
 import * as no_parse from "./build/no_parse/index";
+import { pack } from "./classes/pack"
 import { json_clean } from "./build/json_clean/index"
 import { mpm } from "./build/mpm/index"
 import { score_ns } from "./build/score_ns/index"
@@ -10,12 +12,13 @@ import { tag_ns } from "./build/tag_ns/index"
 import { lootmerge } from "./build/lootmerge/index"
 
 
-
-export function compile(compile_from: string, compile_to: string, in_dev: boolean) {
+export function compile(compile_from: string, compile_to: string, manifest: string, in_dev: boolean) {
     let packs: Array<pack> = [];
     let packID: number = 0;
     const packnames: Array<string> = fs.readdirSync(compile_from, "utf8");
     let assets_formats: Array<number> = [];
+
+    if (fs.existsSync(manifest)) packID = packID + hjson.parse(manifest).lastID + 1;
 
     for (let packname of packnames) packs.push(new pack(path.join(compile_from, packname), packID++));
 
@@ -40,4 +43,4 @@ export function compile(compile_from: string, compile_to: string, in_dev: boolea
     json_clean(compile_to);
 }
 
-compile("./test/compile_from", "./test/compile_to", true); // aight lets try it
+compile("./test/compile_from", "./test/compile_to", "./test/compile_to/manifest.json", true); // aight lets try it
